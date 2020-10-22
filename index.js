73680e0677d9517d2dd6d7cc738cbe89eecb7251
MostafaCse <mdmostafaiiuc@gmail.com>
2020-10-22 23:19:52 +0600

    correct

--- index.js
+++ index.js
@@ -11,7 +11,7 @@ app.get('/',(req,res)=>{
     res.send("hey! i am working");
 })
 
-const uri = `mongodb+srv://${process.env.DB_user}:${process.env.DB_PASS}@cluster0.o2wak.mongodb.net/${process.env.DB_NAME}?retryWrites=true&w=majority`;
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.o2wak.mongodb.net/${process.env.DB_NAME}?retryWrites=true&w=majority`;
 const port = 5000
 const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true  });
 client.connect(err => {
